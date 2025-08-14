bde82f3d1d3874bf79defc19062ea7277581ed1f
Yastchur <Yastchur@gmail.com>
2025-08-14 18:16:43 +0300

    Группировка тестов

--- test_main_page.py
+++ test_main_page.py
@@ -1,20 +1,22 @@
+import pytest
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+        page.open()                      # открываем страницу
+        page.should_be_login_link()      # выполняем проверку наличия переходим на страницу логина
+        page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.should_be_login_link()      # выполняем проверку наличия переходим на страницу логина
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-
-def test_guest_see_login_anr_register_form_on_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.should_be_login_page()      # реализуйте проверку
+    def test_guest_see_login_anr_register_form_on_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+        page.open()                      # открываем страницу
+        page.should_be_login_page()      # реализуйте проверку
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     url = "http://selenium1py.pythonanywhere.com/"
--- test_product_page.py
+++ test_product_page.py
@@ -2,48 +2,52 @@ import pytest
 from pages.product_page import ProductPage
 from pages.basket_page import BasketPage
 
-
-@pytest.mark.xfail(reason="Сообщение появляется после добавления товара — ожидаемое поведение")
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, url)
-    page.open()
-    page.add_product_to_basket()
-    page.should_not_be_success_message()
-
-def test_guest_cant_see_success_message(browser):
-    url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
-    page = ProductPage(browser, url)
-    page.open()
-    page.should_not_be_success_message()
-
-@pytest.mark.xfail(reason="Сообщение не исчезает автоматически — баг или фича")
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, url)
-    page.open()
-    page.add_product_to_basket()
-    page.should_disappear_success_message()
+@pytest.mark.login_on_product_page
+class TestLoginOnProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        url = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        self.page = ProductPage(browser, url)
+        self.page.open()
     
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
-    
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
-    product_page = ProductPage(browser, url)
-    product_page.open()
-    product_page.go_to_basket()
+    def test_guest_should_see_login_link_on_product_page(self):
+        self.page.should_be_login_link()
+        
+    def test_guest_can_go_to_login_page_from_product_page(self):
+        self.page.go_to_login_page()    
 
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_not_be_items_in_basket()
-    basket_page.should_be_empty_basket_text()
+@pytest.mark.success_message_on_product_page
+class TestSuccessMessageOnProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        self.page = ProductPage(browser, url)
+        self.page.open()
+        
+    @pytest.mark.xfail(reason="Сообщение появляется после добавления товара — ожидаемое поведение")
+    def test_guest_cant_see_success_message_after_adding_product_to_basket(self):
+        self.page.add_product_to_basket()
+        self.page.should_not_be_success_message()
+        
+    @pytest.mark.xfail(reason="Сообщение не исчезает автоматически — баг или фича")
+    def test_message_disappeared_after_adding_product_to_basket(self):
+        page.add_product_to_basket()
+        page.should_disappear_success_message()
+
+@pytest.mark.product_page
+class TestSuccessMessageOnProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
+        self.page = ProductPage(browser, url)
+        self.page.open()
+        
+    def test_guest_cant_see_success_message(self):
+        self.page.should_not_be_success_message()
+
+    def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
+        self.page.go_to_basket()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_not_be_items_in_basket()
+        basket_page.should_be_empty_basket_text()
 
